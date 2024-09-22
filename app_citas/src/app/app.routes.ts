a6ff046f4a95670914c01b76cf72eb20eb4be798
benjamin <carlosbpac@gmail.com>
2024-09-21 22:11:33 -0600

    Refactor app component and add routing configuration

--- app_citas/src/app/app.routes.ts
+++ app_citas/src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component:DefaultLayoutComponent,
+    children: [
+      {
+        path: '',component: HomeComponent
+      },
+      {
+        path: 'home',component: HomeComponent
+      },
+      {
+        path: 'login',component: LoginComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- app_citas/src/app/layout/default-layout/default-layout.component.ts
+++ app_citas/src/app/layout/default-layout/default-layout.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
+  standalone: true,
   selector: 'app-default-layout',
+  imports: [NavbarComponent, CommonModule, RouterOutlet],
   templateUrl: './default-layout.component.html',
   styleUrls: ['./default-layout.component.css']
 })
-export class DefaultLayoutComponent implements OnInit {
-
+export class DefaultLayoutComponent {
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
