a97a549484867168c32bf1490e9df6a6e91cde38
Benjamin <carlosbpac@gmail.com>
2024-09-22 14:37:02 -0600

    Add new components for see-court, edit-court, edit-employee, and see-employees

---
+++ app_citas/src/app/views/see-court/see-court.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-court',
+  templateUrl: './see-court.component.html',
+  styleUrls: ['./see-court.component.css']
+})
+export class SeeCourtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
