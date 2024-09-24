cf462998d894880fbdaaca006822fbafc04017e9
Benjamin <carlosbpac@gmail.com>
2024-09-23 21:03:58 -0600

    Refactor card-actions component to use buttons instead of links for actions

--- app_citas/src/app/components/card-actions/card-actions.component.ts
+++ app_citas/src/app/components/card-actions/card-actions.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
+  imports: [CommonModule, RouterModule],
   selector: 'app-card-actions',
   templateUrl: './card-actions.component.html',
   styleUrls: ['./card-actions.component.css']
@@ -11,9 +13,17 @@ export class CardActionsComponent implements OnInit {
   @Input() alt = '';
   @Input() title = '';
   @Input() description = '';
+  @Input() actions = [
+    { description: 'Action 1', route: '/action1', enabled: true },
+    { description: 'Action 2', route: '/action2', enabled: false },
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getEnabledActions() {
+    return this.actions.filter(action => action.enabled);
   }
 
 }
--- app_citas/src/app/views/see-courts/see-courts.component.ts
+++ app_citas/src/app/views/see-courts/see-courts.component.ts
@@ -11,10 +11,44 @@ import { CardActionsComponent } from '../../components/card-actions/card-actions
   styleUrls: ['./see-courts.component.css']
 })
 export class SeeCourtsComponent implements OnInit {
+  canchas = [
+    { id: 1, name: 'Cancha 1', description: 'Cancha de futbol', price: 100 },
+    { id: 2, name: 'Cancha 2', description: 'Cancha de futbol', price: 200 },
+    { id: 3, name: 'Cancha 3', description: 'Cancha de futbol', price: 300 },
+    { id: 4, name: 'Cancha 4', description: 'Cancha de futbol', price: 400 },
+    { id: 5, name: 'Cancha 5', description: 'Cancha de futbol', price: 500 },
+  ];
+  acciones_canchas = [
+    { description: 'Reservar', route: '/reservar-cancha', enabled: true },
+    { description: 'Modificar', route: '/edit-cancha', enabled: true },
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setValueAction(cancha: any): any[] {
+    //Clonamos el arreglo de acciones
+    let acciones:any[] = [];
+
+    //Iteramos el arreglo de acciones para agregar el id de la cancha
+    this.acciones_canchas.forEach(accion => {
+      //Clonamos la accion
+      let accion_clon = { ...accion };
+
+      //Asignamos la ruta de la accion
+      accion_clon.route += `/${cancha.id}`;
+
+      //Asignamos la accion al arreglo de acciones
+      acciones.push(accion_clon);
+    });
+    //Asignamos el nuevo arreglo de acciones
+    return acciones;
+  }
+
+  setDescripcionAction(cancha: any) {
+    return `Reservar la cancha ${cancha.name}`;
   }
 
 }
