241da2da94ef610c33718c89ba347ec2bdd1e5f8
cresb-49 <carlosbpac@gmail.com>
2024-09-24 23:03:53 -0600

    Refactor AuthService and HttpService classes, and add HttpService dependency to AuthService constructor

--- app_citas/src/app/services/auth/auth.service.ts
+++ app_citas/src/app/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpService } from '../http/http.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,30 +14,30 @@ export class AuthService {
   private roles: string[] = []; // Roles del usuario
   private permissions: string[] = []; // Permisos del usuario
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private httpService: HttpService) { }
 
   // Método para iniciar sesión
-  login(email: string, password: string): boolean {
-    // Lógica para validar el inicio de sesión
-    if (email === 'admin@example.com' && password === 'password') {
-      this.isAuthenticated = true;
-      this.roles = ['ADMIN'];
-      this.permissions = ['CREATE', 'READ', 'UPDATE', 'DELETE'];
-      localStorage.setItem('isAuthenticated', 'true');
-      localStorage.setItem('roles', JSON.stringify(this.roles));
-      localStorage.setItem('permissions', JSON.stringify(this.permissions));
-      return true;
-    } else if (email === 'user@example.com' && password === 'password') {
-      this.isAuthenticated = true;
-      this.roles = ['USER'];
-      this.permissions = ['READ'];
-      localStorage.setItem('isAuthenticated', 'true');
-      localStorage.setItem('roles', JSON.stringify(this.roles));
-      localStorage.setItem('permissions', JSON.stringify(this.permissions));
-      return true;
-    }
-    return false;
-  }
+  // login(email: string, password: string): boolean {
+  //   const payload = {
+  //     email: email,
+  //     password: password
+  //   };
+  //   this.httpService.post<any>('auth/login', payload).subscribe(
+  //     {
+  //       next: (data) => {
+  //         console.log('response:', data);
+  //         this.isAuthenticated = true;
+  //         this.saveLocalStorage();
+  //       },
+  //       error: (error) => {
+  //         this.isAuthenticated = false;
+  //         console.error('Error:', error);
+  //         this.clearLocalStorage();
+  //       }
+  //     }
+  //   );
+  //   return this.isAuthenticated;
+  // }
 
   // Método para cerrar sesión
   logout(): void {
@@ -55,23 +56,23 @@ export class AuthService {
   }
 
   getToken(): string {
-    return this.token;
+    return this.token !== '' ? this.token : localStorage.getItem('token') || '';
   }
 
   getName(): string {
-    return this.name;
+    return this.name !== '' ? this.name : localStorage.getItem('name') || '';
   }
 
   getLastname(): string {
-    return this.lastname;
+    return this.lastname !== '' ? this.lastname : localStorage.getItem('lastname') || '';
   }
 
   getEmail(): string {
-    return this.email;
+    return this.email !== '' ? this.email : localStorage.getItem('email') || '';
   }
 
   getFullName(): string {
-    return `${this.name} ${this.lastname}`;
+    return `${this.getName()} ${this.getLastname()}`;
   }
 
   // Obtener roles
@@ -94,13 +95,44 @@ export class AuthService {
     return this.getUserPermissions().includes(permission);
   }
 
-  logOut(): void {
+  private saveLocalStorage(payload: any = null): void {
+    if (payload) {
+      this.token = payload.token;
+      this.name = payload.name;
+      this.lastname = payload.lastname;
+      this.email = payload.email;
+      this.isAuthenticated = payload.isAuthenticated;
+      this.roles = payload.roles;
+      this.permissions = payload.permissions;
+    }
+    localStorage.setItem('token', this.token);
+    localStorage.setItem('name', this.name);
+    localStorage.setItem('lastname', this.lastname);
+    localStorage.setItem('email', this.email);
+    localStorage.setItem('isAuthenticated', this.isAuthenticated.toString());
+    localStorage.setItem('roles', JSON.stringify(this.roles));
+    localStorage.setItem('permissions', JSON.stringify(this.permissions));
+  }
+
+  private clearLocalStorage(): void {
+    this.token = '';
+    this.name = '';
+    this.lastname = '';
+    this.email = '';
     this.isAuthenticated = false;
     this.roles = [];
     this.permissions = [];
-    localStorage.removeItem('isAuthenticated');
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    localStorage.removeItem('lastname');
+    localStorage.removeItem('email');
     localStorage.removeItem('roles');
     localStorage.removeItem('permissions');
+    localStorage.setItem('isAuthenticated', 'false');
+  }
+
+  logOut(): void {
+    this.clearLocalStorage();
     this.router.navigate(['/login']);
   }
 }
