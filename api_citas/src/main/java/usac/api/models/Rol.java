8634703b13aec31e7683f15911f6fed1d1cc1aa4
cresb-49 <carlosbpac@gmail.com>
2024-09-24 20:37:05 -0600

    Refactor Rol model to add entity and table annotations

--- api_citas/src/main/java/usac/api/models/Rol.java
+++ api_citas/src/main/java/usac/api/models/Rol.java
@@ -4,22 +4,34 @@
  */
 package usac.api.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  *
  * @author Luis Monterroso
  */
+@Entity
+@Table(name = "rol")
+@SQLDelete(sql = "UPDATE rol SET deleted_at = NULL WHERE id = ?")
+@Where(clause = "desactivated_at IS NULL")
 public class Rol extends Auditor {
 
     @Column(length = 250, nullable = false)
