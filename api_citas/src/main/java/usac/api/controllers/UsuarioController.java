185c9bfb08bb6a770fcccba03619a94feea45349
cresb-49 <carlosbpac@gmail.com>
2024-09-29 15:32:06 -0600

    Refactor authentication service to store user ID in local storage

--- api_citas/src/main/java/usac/api/controllers/UsuarioController.java
+++ api_citas/src/main/java/usac/api/controllers/UsuarioController.java
@@ -4,20 +4,12 @@
  */
 package usac.api.controllers;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +17,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import usac.api.models.Usuario;
 import usac.api.models.dto.LoginDTO;
+import usac.api.models.dto.UserChangePasswordDTO;
 import usac.api.models.request.PasswordChangeRequest;
 import usac.api.services.UsuarioService;
 import usac.api.tools.transformers.ApiBaseTransformer;
@@ -123,7 +124,7 @@ public class UsuarioController {
     })
     @PatchMapping("/protected/cambiarPassword")
     public ResponseEntity<?> cambiarPassword(
-            @Parameter(description = "ID del producto a buscar", required = true, example = "{id:1,password:\"xd\"}") @RequestBody Usuario requestBody) {
+            @Parameter(description = "ID del producto a buscar", required = true, example = "{id:1,password:\"xd\",newPassword:\"xdnt\"}") @RequestBody UserChangePasswordDTO requestBody) {
         try {
             String respuesta = usuarioService.cambiarPassword(requestBody);
             return new ApiBaseTransformer(HttpStatus.OK, "OK", respuesta, null, null).sendResponse();
