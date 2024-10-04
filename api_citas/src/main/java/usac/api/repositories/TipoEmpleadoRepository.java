74f5aae7bbc4b8ef973ebd88cdc6b46650a5bf5f
cresb-49 <carlosbpac@gmail.com>
2024-10-04 01:57:18 -0600

    Add CanchaRepository and TipoEmpleadoRepository

--- api_citas/src/main/java/usac/api/repositories/TipoEmpleadoRepository.java
+++ api_citas/src/main/java/usac/api/repositories/TipoEmpleadoRepository.java
@@ -1,10 +1,12 @@
 package usac.api.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import usac.api.models.TipoEmpleado;
 
 import java.util.Optional;
 
+@Repository
 public interface TipoEmpleadoRepository extends CrudRepository<TipoEmpleado, Long> {
     public Optional<TipoEmpleado> findOneByNombre(String nombre);
 }
---
+++ api_citas/src/main/java/usac/api/services/HorarioCanchaService.java
@@ -0,0 +1,25 @@
+package usac.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usac.api.models.Cancha;
+import usac.api.models.Dia;
+import usac.api.models.HorarioCancha;
+import usac.api.repositories.HorarioCanchaRepository;
+
+import java.util.List;
+
+@Service
+public class HorarioCanchaService extends usac.api.services.Service {
+    @Autowired
+    private HorarioCanchaRepository horarioCanchaRepository;
+
+    public List<HorarioCancha> obtenerTodosLosHorariosIncluyendoEliminados(Cancha cancha) {
+        return horarioCanchaRepository.findAllByCanchaIncludingDeleted(cancha);
+    }
+
+    public HorarioCancha obtenerHorarioPorDiaYCancha(Dia dia, Cancha cancha) {
+        return horarioCanchaRepository.findByDiaAndCancha(dia, cancha);
+    }
+
+}
