daa7ab896b686e43cb349ded8a9da6ce40daa69b
Benjamin <carlosbpac@gmail.com>
2024-10-09 17:22:47 -0600

    Refactor employee-related code and remove unused type
    - Remove TipoEmpleadoRepository and TipoEmpleado model
    - Remove references to TipoEmpleado in UsuarioService and EmpleadoService
    - Update Empleado constructor and setter methods in Empleado model
    - Update Servicio model to use Rol instead of TipoEmpleado
    - Update references to TipoEmpleado in NuevoEmpleadoRequest model

--- api_citas/src/main/java/usac/api/services/EmpleadoService.java
+++ api_citas/src/main/java/usac/api/services/EmpleadoService.java
@@ -8,38 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import usac.api.models.Empleado;
-import usac.api.models.TipoEmpleado;
 import usac.api.repositories.EmpleadoRepository;
-import usac.api.repositories.TipoEmpleadoRepository;
 
 @Service
 public class EmpleadoService extends usac.api.services.Service {
     @Autowired
-    private TipoEmpleadoRepository tipoEmpleadoRepository;
-    @Autowired
     private EmpleadoRepository empleadoRepository;
 
-    @Transactional(rollbackOn = Exception.class)
-    public TipoEmpleado createTipoEmpleado(TipoEmpleado tipoEmpleado) throws Exception {
-        //Validamos el modelo
-        this.validarModelo(tipoEmpleado);
-        //Creamos el tipo de empleado
-        TipoEmpleado tipoCreado = tipoEmpleadoRepository.save(tipoEmpleado);
-        if (tipoCreado != null && tipoCreado.getId() != null) {
-            return tipoCreado;
-        }
-        throw new Exception("No se pudo crear el tipo de empleado");
-    }
-
-    public TipoEmpleado getTipoEmpleadoByNombre(String nombre) throws Exception {
-        TipoEmpleado tipoEmpleado = tipoEmpleadoRepository.findOneByNombre(nombre).orElse(null);
-        if (tipoEmpleado == null) {
-            throw new Exception("Tipo de empleado no encontrado");
-        }
-        return tipoEmpleado;
-    }
-
-    @Deprecated
     @Transactional(rollbackOn = Exception.class)
     public Empleado createEmpleado(Empleado empleado) throws Exception {
         //Validamos el modelo
