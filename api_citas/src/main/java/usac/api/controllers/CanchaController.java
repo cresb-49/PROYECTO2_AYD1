51bade9c40040e5c09fd5da7bceb30c345f3dc2f
Benjamin <carlosbpac@gmail.com>
2024-10-15 00:03:55 -0600

    Add endpoint for creating a court in CanchaController

--- api_citas/src/main/java/usac/api/controllers/CanchaController.java
+++ api_citas/src/main/java/usac/api/controllers/CanchaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,18 @@ public class CanchaController {
     public ResponseEntity<?> getCanchaById(@PathVariable Long id) {
         try {
             Cancha data = canchaService.getCanchaById(id);
+            return new ApiBaseTransformer(HttpStatus.OK, "OK", data, null, null).sendResponse();
+        } catch (Exception ex) {
+            return new ApiBaseTransformer(HttpStatus.BAD_REQUEST, "Error", null, null, ex.getMessage()).sendResponse();
+        }
+    }
+
+    @PostMapping("/private/cancha")
+    public ResponseEntity<?> createCancha(@RequestBody CanchaRequest canchaRequest) {
+        try {
+            Cancha cancha = canchaRequest.getCancha();
+            List<HorarioCancha> horarios = canchaRequest.getHorarios();
+            Cancha data = canchaService.crearCancha(cancha, horarios);
             return new ApiBaseTransformer(HttpStatus.OK, "OK", data, null, null).sendResponse();
         } catch (Exception ex) {
             return new ApiBaseTransformer(HttpStatus.BAD_REQUEST, "Error", null, null, ex.getMessage()).sendResponse();
