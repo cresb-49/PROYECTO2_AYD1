8ebe7a2a6ddc808267a396eb86c6719c22eea962
Benjamin <carlosbpac@gmail.com>
2024-10-20 10:21:44 -0600

    Refactor B64Service to improve image handling
    This commit refactors the B64Service class in order to improve the handling of images. The changes include:
    - Adding import for java.util.Base64 to use Base64 encoding and decoding
    - Updating comments to provide clearer explanations of the code logic
    - Adding a new method, base64ToByteArray, to convert a Base64 string to a byte array
    
    These changes aim to enhance the functionality and maintainability of the B64Service class.

--- api_citas/src/main/java/usac/api/services/B64Service.java
+++ api_citas/src/main/java/usac/api/services/B64Service.java
@@ -1,17 +1,34 @@
 package usac.api.services;
 
-@org.springframework.stereotype.Service
-public class B64Service extends Service{
-    
+import java.util.Base64;
 
-    //En esta funcion validamos que la cadena de string tenga el inicio
-    //data:image/png;base64, para que el navegador pueda interpretar la imagen
+@org.springframework.stereotype.Service
+public class B64Service extends Service {
+
+    // En esta funcion validamos que la cadena de string tenga el inicio
+    // data:image/png;base64, para que el navegador pueda interpretar la imagen
     public boolean hasExtension(String base64) {
         return base64.startsWith("data:image");
     }
 
-    //En esta funcion agregamos el inicio data:image/png;base64, a la cadena de string
+    // En esta funcion agregamos el inicio data:image/png;base64, a la cadena de
+    // string
     public String addExtension(String base64) {
         return "data:image/png;base64," + base64;
+    }
+
+    /**
+     * Convierte una cadena Base64 a un arreglo de bytes.
+     *
+     * @param base64Image La cadena en formato Base64 que representa la imagen.
+     * @return Un arreglo de bytes que representa la imagen.
+     */
+    public static byte[] base64ToByteArray(String base64Image) {
+        // Eliminar el prefijo 'data:image/*;base64,' si es necesario
+        if (base64Image.contains(",")) {
+            base64Image = base64Image.split(",")[1];
+        }
+        // Decodificar la cadena Base64
+        return Base64.getDecoder().decode(base64Image);
     }
 }
