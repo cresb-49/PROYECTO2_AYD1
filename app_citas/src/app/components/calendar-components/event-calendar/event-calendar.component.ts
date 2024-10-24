e9f6d05be69e14e877e96fb07e139d5e5ee425c3
Benjamin <carlosbpac@gmail.com>
2024-10-24 03:53:01 -0600

    Refactor code to update event-calendar component with dynamic event display and dropdown functionality

--- app_citas/src/app/components/calendar-components/event-calendar/event-calendar.component.ts
+++ app_citas/src/app/components/calendar-components/event-calendar/event-calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,6 +12,7 @@ export class EventCalendarComponent implements OnInit {
   mes: string = 'Enero';
   dia: string | number = 10;
   year: string | number = 2020;
+  isDropdownOpen: boolean = false; // Control del estado del dropdown
 
   @Input() isCita: boolean = true;
   @Input() fecha: string = '2020-01-10';
@@ -26,10 +27,19 @@ export class EventCalendarComponent implements OnInit {
     'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
   ];
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.desgloseFecha(this.fecha);
+
+    // Listener global para detectar clics fuera del dropdown
+    this.renderer.listen('document', 'click', (event: Event) => {
+      const targetElement = event.target as HTMLElement;
+      // Si el dropdown está abierto y el clic ocurre fuera del dropdown, se cierra
+      if (this.isDropdownOpen && !this.elRef.nativeElement.contains(targetElement)) {
+        this.isDropdownOpen = false;
+      }
+    });
   }
 
   desgloseFecha(fechaStr: string) {
@@ -59,5 +69,21 @@ export class EventCalendarComponent implements OnInit {
   getMes(mesStr: string): string {
     const mesIndex = parseInt(mesStr, 10) - 1; // Restamos 1 porque los meses en el array comienzan en 0
     return this.meses[mesIndex] || 'Mes inválido'; // Si no existe, devolverá un mensaje de error
+  }
+
+  // Método para alternar el estado del dropdown
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // Manejar acciones de edición y eliminación
+  cancelar() {
+    console.log('Cancelar clicked');
+    this.isDropdownOpen = false; // Cerrar el dropdown después de la acción
+  }
+
+  procesar() {
+    console.log('Procesar clicked');
+    this.isDropdownOpen = false; // Cerrar el dropdown después de la acción
   }
 }
