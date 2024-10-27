bcafb5d343aebc3b31f2e2ce9d35c8e76b8ca10a
Benjamin <carlosbpac@gmail.com>
2024-10-26 20:36:09 -0600

    Refactor event-calendar.component.ts to conditionally download a factura based on id

--- app_citas/src/app/views/procesar-cita/procesar-cita.component.ts
+++ app_citas/src/app/views/procesar-cita/procesar-cita.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { ReservaService } from '../../services/reserva/reserva.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute } from '@angular/router';
 @Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   selector: 'app-procesar-cita',
   templateUrl: './procesar-cita.component.html',
   styleUrls: ['./procesar-cita.component.css']
 })
 export class ProcesarCitaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private reservaService: ReservaService,
+    private toastr: ToastrService,
+    private route: ActivatedRoute
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cargarDatosRerserva();
+  }
+
+  cargarDatosRerserva() {
+    //Obtenemos el id de la reserva en la url de la pagina
+    const id_reserva = this.route.snapshot.paramMap.get('id');
+    console.log("id_reserva", id_reserva);
   }
 
 }
